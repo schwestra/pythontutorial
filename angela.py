ee867d5649212d4bc290f84494f3d2b6a60b52d0
schwestra <araskoclu@gmail.com>
2023-03-03 17:41:21 +0300

    first commit

---
+++ angela.py
@@ -0,0 +1,77 @@
+# bill = float(input('What was the total bill? '))
+# tip = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
+# people = int(input('How many people to split to bill? '))
+
+# bahsis = tip / 100
+# bahsis2 = bahsis * bill
+
+# kisi = bill + bahsis2
+
+# kisibasi = kisi / people
+
+# final = round(kisibasi, 2)
+
+# print(f"Each person should pay {final} ")
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# # 🚨 Don't change the code below 👇
+# number = int(input("Which number do you want to check? "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+
+# if number % 2:
+#     print("This is an odd number.")
+# else:
+#     print("This is an even number.") 
+
+#--------------------------------------------------------------------------------------------------
+
+# luna = int(input("Giriş yapmak için boyunuzu giriniz: "))
+# yas = int(input("Yaşınızı Giriniz: "))
+
+# if luna >= 120:
+#     print('Giriş yapabilirsiniz')
+#     if yas <= 18:
+#      print('Fiyat 7$'):
+#     if yas > 18:
+#      print('Fiyat 12$') 
+
+
+    
+# else:
+#     print('Giriş yapamazsınız') 
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+bmi = weight / ( height ** 2)
+bmi2 = round(bmi)
+
+if bmi2 < 18.5:
+    print(f"Your BMI is {bmi2}, you are underweight.")
+elif bmi2 < 25:
+    print(f"Your BMI is {bmi2}, you have a normal weight.")
+elif bmi2 < 30:
+    print(f"Your BMI is {bmi2}, you are slightly overweight.")
+elif bmi2 < 35:
+    print(f"Your BMI is {bmi2}, you are obese.")
+else:
+    print(f"Your BMI is {bmi2}, you are clinically obese.")
+
+
+
+    
+    
+    
+   
+
+
---
+++ deneme.py
@@ -0,0 +1,47 @@
+# name, surname, age, job = 'Bora','Yılmaz', 32, 'mühendis'
+# print(f'Benim adım {name} {surname}, Yaşım {age} ve mesleğim {job}.')
+
+# s = '22345' * 5
+# print(s[::8])
+
+# message = 'Python is gay'
+# print(message.startswith('Pythonn'))
+
+# course = 'Python Kursu: Baştan Sonra Python Programlama Rehberiniz (40 saat)'
+# website = 'http://www.sadikturan.com'
+# # x = course.isalpha()
+# # print(x)
+
+# # course = 'Pythron Kursu: Baştan Sonra Python Programlama Rehberiniz (40 saat)'
+
+# # result = course.replace(' ', '*')
+# # print(result)
+
+# # isTrue = startswith
+# result = website.startswith('www')
+# result2 = website.endswith('com')
+# print(result) 
+# print(result2)
+
+
+# boy = int(input("Giriş yapmak için boyunuzu giriniz: "))
+
+
+
+# if boy > 120:
+#    print('Giriş yapabilirsiniz.')
+
+#    yas = int(input("Yaşınızı Giriniz: "))   
+#    if yas < 12:
+#       print('5$')
+#    elif yas > 12:
+#       print('Ücret 7$')
+#    elif yas < 18:
+#       print('7 $')
+# else:
+#    print("Can't ride")
+
+#--------------------------------------------------------------------------------------------
+
+
+
